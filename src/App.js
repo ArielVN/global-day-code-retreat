2f63d13d75942ca4726540c7f7a523b7aa815273
Ariel NONO <ariel.nono-njanmou@capgemini.com>
2020-11-07 10:48:08 +0100

    iteration 1 - code retreat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const isDeadAndHas3Neighbours = (isAlive, numberOfNeighbours) => !isAlive && numberOfNeighbours === 3;
+const isAliveAndHas2Or3Neighbours = (isAlive, numberOfNeighbours) => isAlive && (numberOfNeighbours === 2 || numberOfNeighbours === 3);
 
-export default App;
+export const isCellAlive = (isAlive, numberOfNeighbours) => isAliveAndHas2Or3Neighbours(isAlive, numberOfNeighbours) || isDeadAndHas3Neighbours(isAlive, numberOfNeighbours);
+
+export const getNumberOfAlivedNeighbours = (map, x, y) => 0;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { isCellAlive } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('isCellAlive function', () => {
+  it('should return false if the cell is alive and has less than 2 neighbours', () => {
+    expect(isCellAlive(true, 1)).toBeFalsy();
+  });
+  it('should return true if the cell is alive and has 2 neighbours', () => {
+    expect(isCellAlive(true, 2)).toBeTruthy();
+  });
+  it('should return true if the cell is alive and has 3 neighbours', () => {
+    expect(isCellAlive(true, 3)).toBeTruthy();
+  });
+  it('should return false if the cell is alive and has more than 3 neighbours', () => {
+    expect(isCellAlive(true, 4)).toBeFalsy();
+  });
+  it('should return true if the cell is dead and has 3 neighbours', () => {
+    expect(isCellAlive(false, 3)).toBeTruthy();
+  });
+})
+
+describe('Name of the group', () => {
+   const world =  new Map();
+   world.set({x:1, y:1}, true)
 });
