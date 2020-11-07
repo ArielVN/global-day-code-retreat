5f907bbee8170c7ae7c2721fb827db7b40d1e461
Ariel NONO <ariel.nono-njanmou@capgemini.com>
2020-11-07 14:19:41 +0100

    sans video

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+const { Grid } = require("./App");
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('', () => {
+  it('should return an empty grid when the grid is empty', () => {
+    const grid = new Grid();
+    expect(grid.nouvelleIteration().estVide()).toBeTruthy();
+  });
+  it('should return an empty grid when the grid as one cell', () => {
+    const grid = new Grid();
+    grid.faitNaitreUneCellule();
+    expect(grid.nouvelleIteration().estVide()).toBeTruthy();
+  });
+  it('should return a grid with one cell when the grid as one cell with 2 Neigbours', () => {
+    const grid = new Grid();
+    grid.faitNaitreUneCellule();
+    expect(grid.nouvelleIteration().estVide()).toBeTruthy();
+  });
 });
